40725600357e2848a56b644610087600f750e651
chosh31 <sunghwancho2011@gmail.com>
2017-04-22 20:03:18 +0900

    use state and click event

--- src/kanban.js
+++ src/kanban.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import KanbanBoard from './KanbanBoard';
-import styles from './kanban.css';
+import './kanban.css';
 
 let cardsList = [
     {
